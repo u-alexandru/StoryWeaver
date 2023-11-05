27e40455bb1a0374ebbecf4caa4cd3166a23f910
Alexandru <u95.alexandru@gmail.com>
2023-11-05 21:31:44 +0100

    Ready for development

--- buildenvironment.sh
+++ buildenvironment.sh
@@ -7,19 +7,19 @@
 # clone develop branch from git@github.com:u-alexandru/StoryWeaver_Frontend.git
 # if Frontend folder exists then update it
 # else clone it
-if [ -d "StoryWeaver_Frontend" ]; then
-    echo "StoryWeaver_Frontend folder exists"
+if [ -d "StoryWeaver_V2_Frontend" ]; then
+    echo "StoryWeaver_V2_Frontend folder exists"
 else
-    git clone -b develop git@github.com:u-alexandru/StoryWeaver_Frontend.git
-    chmod -R 777 StoryWeaver_Frontend
+    git clone -b develop git@github.com:u-alexandru/StoryWeaver_V2_Frontend.git
+    chmod -R 777 StoryWeaver_V2_Frontend
 fi
 
-if [ -d "StoryWeaver_Backend_Laravel" ]; then
-    echo "StoryWeaver_Backend_Laravel folder exists"
+if [ -d "StoryWeaver_V2_Backend" ]; then
+    echo "StoryWeaver_V2_Backend folder exists"
 else
-  echo "Cloning StoryWeaver_Backend_Laravel"
-    git clone -b develop git@github.com:u-alexandru/StoryWeaver_Backend_Laravel.git
-    chmod -R 777 StoryWeaver_Backend_Laravel
+  echo "Cloning StoryWeaver_V2_Backend"
+    git clone -b develop git@github.com:u-alexandru/StoryWeaver_V2_Backend.git
+    chmod -R 777 StoryWeaver_V2_Backend
 fi
 
 # run docker compose
